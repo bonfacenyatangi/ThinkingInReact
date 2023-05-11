527798606b36ffdc2d1f75c215038915d01abfe3
bonface nyatangi <bonfacenyatangi@gmail.com>
2023-05-11 13:03:35 +0300

    Separated components

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { products } from "./data.js";
-
+import FilterableProductTable from "./FilterableProductTable"
+import { products } from "./data"
 /*
 Thinking in React: https://reactjs.org/docs/thinking-in-react.html
 
@@ -22,72 +22,6 @@ Step 3: Identify The Minimal (but complete) Representation Of UI State
 Step 4: Identify Where Your State Should Live
 Step 5: Add Inverse Data Flow
 */
-
-function ProductCategoryRow({ category }) {
-  return (
-    <tr style={{ fontWeight: "bold" }}>
-      <td colSpan="2">{category}</td>
-    </tr>
-  )
-}
-
-function ProductRow({ price, stocked, name }) {
-  const style = stocked ? null : { color: "red", fontWeight: "bold" }
-  return (
-    <tr>
-      <td style={style}>{name}</td>
-      <td>{price}</td>
-    </tr>
-  )
-}
-
-function ProductTable({ products }) {
-
-  const rows = [];
-  let lastCategory;
-  for (const product of product.category(products)) {
-    if (product.category !== lastCategory) {
-      rows.push(<ProductCategoryRow key={product.category} category={product.category} />)
-    }
-    rows.push(<ProductRow key={product.name} name={product.name} stocked={product.stocked} price={product.price} />)
-    lastCategory = product.category
-  }
-
-  return (
-    <table>
-      <thead>
-        <tr>
-          <th>Name</th>
-          <th>Price</th>
-        </tr>
-      </thead>
-      <tbody>
-        {rows}
-      </tbody>
-    </table>
-  )
-}
-
-function SearchBar() {
-  return (
-    <form style={{ padding: "10px", display: "flex", flexDirection: "column" }}>
-      <input type="text" placeholder="Search..." />
-      <label>
-        <input type="checkbox" />
-        Only show products in stock
-      </label>
-    </form>
-  )
-}
-
-function FilterableProductTable({ products }) {
-  return (
-    <div>
-      <SearchBar />
-      <ProductTable products={products} />
-    </div>
-  )
-}
 
 function App() {
   return (
